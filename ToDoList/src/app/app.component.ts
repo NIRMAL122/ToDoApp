1c9c1fe296a0eee8ad3933e9a725e8636c2b5e99
Nirmal122 <singhnirmal588@gmail.com>
2023-05-06 12:55:53 +0530

    Final Commit v1.0.0

--- ToDoList/src/app/app.component.ts
+++ ToDoList/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Guid} from "guid-typescript"
+import {Todo} from '../Models/todomodel'
+import{NgForm} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDoList';
+  todos:Todo[]= [
+    new Todo(Guid.create(), 'wash Car', false),
+    new Todo(Guid.create(), 'buy Car', false),
+]
+
+onSubmit(form:NgForm){
+  let todo=new Todo(Guid.create(),form.value.title, false)
+  this.todos.push(todo)
+  form.resetForm()
+}
+
+onComplete(id:Guid){
+  let todo=this.todos.filter(x=>x.id==id)[0];
+  todo.isComplete=true;
+}
+onDelete(id:Guid){
+  let todo=this.todos.filter(x=>x.id==id)[0];
+  let index=this.todos.indexOf(todo,0)
+  if(index>-1){
+    this.todos.splice(index,1);
+  }
+}
 }
